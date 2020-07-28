0158bbc423a8c83bf0e58b709ce9e3f85c858b54
Sasha Evtushenko <sasha.evtushenko.md@gmail.com>
2020-07-28 17:55:28 +0300

    Working app featuring MVC design pattern

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var trueButton: UIButton!
+    @IBOutlet var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.systemGreen
+        } else {
+            sender.backgroundColor = UIColor.systemRed
+        }
+        
+        quizBrain.nextQuestion()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.updateUI()
+        }
+    }
+    
+    func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+    
+}
+
